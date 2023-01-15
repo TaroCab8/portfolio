f2a2d085bc4a1e2dd990be36a619be2f0df0947b
Lautaro F Cabrera <admin@iMac.local>
2023-01-15 15:15:12 +0100

    Initial commit#

---
+++ template.js
@@ -0,0 +1,14 @@
+export default function Template () {
+    `
+        <doctype html>
+        <meta="utf-8"
+        <head>
+            <title>Lautaro Fabian Cabrera</title>
+        </head>
+        <body>
+            <div id="root">
+                <h1>"Hola conchatumare"</h1>
+            </div>
+        </body>
+    `
+}
