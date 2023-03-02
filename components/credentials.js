09b8708e5b4a17a7c95f35964da145b101b5f541
TaroCab8 <lautacabrera@gmail.com>
2023-03-02 18:57:53 +0100

    beta version

---
+++ components/credentials.js
@@ -0,0 +1,9 @@
+const credentials= {
+    mailgun: {
+        service_id: "service_djvhkqs",
+        template_id: "template_3ckmovr",
+        user_id: "s_xbktrLowWfBmOFY"
+    }
+}
+
+export default credentials
