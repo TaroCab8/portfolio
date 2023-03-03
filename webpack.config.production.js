1a9ea961a1ecc80a1b61f8bdca633d2308761da8
TaroCab8 <lautacabrera@gmail.com>
2023-03-03 14:22:37 +0100

    3nd deploy

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -8,7 +8,7 @@ const config = {
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "main.js",
-        publicPath:'/build/',
+        publicPath:'/',
     },
     devServer:{
         static: ["./public"],
