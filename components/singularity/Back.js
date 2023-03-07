fb1cd9bc6f70cf516602f34a16d23cad30b361da
TaroCab8 <lautacabrera@gmail.com>
2023-03-07 17:06:23 +0100

    repair

---
+++ components/singularity/Back.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Fondo() {
+const arraY=[   ]
+    return(
+        <div>
+            {
+                arraY.map((item, i)=> {
+                    
+                })
+            }
+        </div>
+    )
+}
